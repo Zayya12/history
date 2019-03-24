6096414f7b12638cc75133d336519b54fe4d9e9a
Mask Killer <41992180+Zayya12@users.noreply.github.com>
2019-03-24 21:55:44 +0630

    Update imageslider.php

--- imageslider.php
+++ imageslider.php
@@ -1,20 +1,20 @@
 [
     {
         "id": "1",
-        "title": "Slide 1",
+        "title": "Pubg Mobile Download",
         "description": "First of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide1.png"
+        "image_url": "https://cdn.guidingtech.com/imager/media/assets/212753/Top-N-PUBG-Wallpapers-in-Full-HD-for-PC-and-Phone-2_4d470f76dc99e18ad75087b1b8410ea9.jpg"
     },
     {
         "id": "2",
-        "title": "Slide 2",
+        "title": "Pubg Pc Lite Download",
         "description": "Second of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide2.png"
+        "image_url": "https://cdn.guidingtech.com/imager/media/assets/212758/Top-N-PUBG-Wallpapers-in-Full-HD-for-PC-and-Phone-7_4d470f76dc99e18ad75087b1b8410ea9.jpg"
     },
     {
         "id": "3",
-        "title": "Slide 3",
+        "title": "Pubg Stream Pc Version Download",
         "description": "Third of three slide",
-        "image_url": "http:\/\/localhost\/sliderimg\/slide3.png"
+        "image_url": "https://cdn.guidingtech.com/imager/media/assets/212751/Top-N-PUBG-Wallpapers-in-Full-HD-for-PC-and-Phone-1_4d470f76dc99e18ad75087b1b8410ea9.jpg"
     }
 ]
